1349613800bbdff81a2d488cd93af4a636e3452f
ZMeharg <ZMeharg@OlivetCollege.edu>
2017-10-18 19:15:36 -0400

    The updated version of the program

--- Balancing Game Code.py
+++ Balancing Game Code.py
@@ -18,35 +18,32 @@ def add_ball(space):
     space.add(body, shape)
     return shape
 
-def add_rectangle(space):
-    """Add a rectangle with random heights and widths."""
-    mass = 1
-    h = random.randint(2, 10)
-    w = random.randit(2,10)
-    inertia = pymunk.moment_for_box(mass, w, h)
-    body = pymunk.Body(mass, inertia)
-    x = random.
+# def add_rectangle(space):
+#     """Add a rectangle with random heights and widths."""
+#     mass = 1
+#     h = random.randint(2, 10)
+#     w = random.randit(2,10)
+#     inertia = pymunk.moment_for_box(mass, w, h)
+#     body = pymunk.Body(mass, inertia)
+#     x = random.
 
 def add_L(space):
     """Add a inverted L shape with two joints"""
     rotation_center_body = pymunk.Body(body_type=pymunk.Body.STATIC)
     rotation_center_body.position = (300, 300)
 
-    rotation_limit_body = pymunk.Body(body_type=pymunk.Body.STATIC)
-    rotation_limit_body.position = (200, 300)
+    # rotation_limit_body = pymunk.Body(body_type=pymunk.Body.STATIC)
+    # rotation_limit_body.position = (200, 300)
 
     body = pymunk.Body(10, 10000)
     body.position = (300, 300)
     l1 = pymunk.Segment(body, (-150, 0), (255.0, 0.0), 5.0)
     # l2 = pymunk.Segment(body, (-150.0, 0), (-150.0, 50.0), 5.0)
 
-    rotation_center_joint = pymunk.PinJoint(
-        body, rotation_center_body, (0, 0), (0, 0))
-    joint_limit = 25
-    rotation_limit_joint = pymunk.SlideJoint(
-        body, rotation_limit_body, (-100, 0), (0, 0), 0, joint_limit)
-
-    space.add(l1, body, rotation_center_joint, rotation_limit_joint)
+    rotation_center_joint = pymunk.GrooveJoint(body, rotation_center_body, (0, 0), (0, 0))
+    joint_1 = pymunk.GrooveJoint(body, rotation_center_body, (-100, 0), (0, 0))
+    joint_2 = pymunk.GrooveJoint(body, rotation_center_body, (200, 0), (0, 0))
+    space.add(l1, body, rotation_center_joint, joint_1, joint_2)
     return l1 
 
 
@@ -84,9 +81,14 @@ def main():
             if ball.body.position.y < 150:
                 balls_to_remove.append(ball)
 
-        for ball in balls_to_remove:
-            space.remove(ball, ball.body)
-            balls.remove(ball)
+        ticks_to_next_ball -= 1
+        if ticks_to_next_ball <= 0:
+            ticks_to_next_ball = 25
+            ball_shape = add_ball(space)
+            balls.append(ball_shape)
+        # for ball in balls_to_remove:
+        #     space.remove(ball, ball.body)
+        #     balls.remove(ball)
 
         space.debug_draw(draw_options)
 
