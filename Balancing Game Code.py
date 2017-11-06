448bd999b548e6972151bba7ee6569e63aebdf31
ZMeharg <ZMeharg@OlivetCollege.edu>
2017-11-06 18:59:49 -0500

    Well this one is interesting, don't use plz.

--- Balancing Game Code.py
+++ Balancing Game Code.py
@@ -34,22 +34,23 @@ def add_box(space):
     h = random.randint(10, 20)
     w = random.randint(10, 20)
     size = (h, w)
-    friction = 1.0
     inertia = pymunk.moment_for_box(mass, size)
     body = pymunk.Body(mass, inertia)
-    x = random.randint(200, 400)
-    for event in pygame.event.get(): 
-        if event.type == pygame.KEYDOWN:
-            pressed = pygame.key.get_pressed()
-            x = 200
-            if pressed[pygame.k_LEFT]:
-                body.x -= 10
-            if pressed[pygame.k_RIGHT]:
-                body.x += 10
-    body.position = x, 550
+    body.position.x = 300
+    body.position = (300, 500)
     box = pymunk.Poly(body, [(-w, -h), (-w,h), (w,-h), (w,h)])
+    box.friction = 10
     space.add(body, box)
     return box
+
+def move_box():
+    pressed = pygame.key.get_pressed()
+    if pressed[k_RIGHT]:
+        body.positoin.x += 10
+    if pressed[k_LEFT]:
+        body.position.x -= 10
+
+
 
 def add_L(space):
     """Add a inverted L shape with two joints"""
@@ -61,12 +62,13 @@ def add_L(space):
     body = pymunk.Body(10, 10000)
     body.position = (300, 300)
     l1 = pymunk.Segment(body, (-150, 0), (150, 0.0), 5.0)
+    l1.friction = 1
     # l2 = pymunk.Segment(body, (0, 0), (0, -400), 5.0)
     # l3 = pymunk.Segment(body, (-150, 0), (0, -200), 5.0)
     
 
     rotation_center_joint = pymunk.constraint.PinJoint(body, rotation_center_body, anchor_a=(0,0), anchor_b=(0,0))
-    rotation_main_joint = pymunk.constraint.PinJoint(body, rotation_main_body, anchor_a=(0,-200), anchor_b=(0, -100))
+    rotation_main_joint = pymunk.constraint.PinJoint(body, rotation_main_body, anchor_a=(0,-150), anchor_b=(0, -50))
     # joint_1 = pymunk.constraint.PinJoint(body, rotation_center_body, anchor_a=(0,-200), anchor_b=(0,0))
     # joint_2 = pymunk.constraint.PinJoint(body, rotation_center_body, anchor_a=(0, -200), anchor_b=(50,-200))
     # rotation_center_joint = pymunk.GrooveJoint(body, rotation_center_body, (0, 0), (0, 0))
@@ -87,35 +89,32 @@ def main():
     space.gravity = (0.0, -900.0)
 
     lines = add_L(space)
-    balls = []
+    boxs = []
     draw_options = pymunk.pygame_util.DrawOptions(screen)
 
-    ticks_to_next_ball = 15
     while True:
-        Activation_Keys()
+        # Activation_Keys()
 
         for event in pygame.event.get():
             if event.type == KEYDOWN and event.key == K_D:
-                ball_shape = add_box(space)
-                balls.append(ball_shape)
-                space.add(balls)
-            move_box()
+                box_shape = add_box(space)
+                boxes.append(box_shape)
+                space.add(boxs)
+                move_box()
 
         screen.fill((255, 255, 255))
 
-        balls_to_remove = []
-        for ball in balls:
-            if ball.body.position.y < 150:
-                balls_to_remove.append(ball)
+        # for box in boxs:
+        #     if box.body.position.y < 100:
+        #         sys.exit(0)
 
-        ticks_to_next_ball -= 1
-        if ticks_to_next_ball <= 0:
-            ticks_to_next_ball = 25
-            ball_shape = add_box(space)
-            balls.append(ball_shape)
-        for ball in balls_to_remove:
-            space.remove(ball, ball.body)
-            balls.remove(ball)
+
+        ticks_to_next_box = 0
+
+        if ticks_to_next_box <= 100:
+            ticks_to_next_box =+ 1
+            box_shape = add_box(space)
+            boxs.append(box_shape)
 
         space.debug_draw(draw_options)
 
