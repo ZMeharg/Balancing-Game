1754168884a3de7035b7e6dc82ae61491308f9a6
ZMeharg <ZMeharg@OlivetCollege.edu>
2017-11-29 19:10:53 -0500

    Balancing Code Game
    In this update I have made it so a box will fall when the Space button
    is pressed, I am working on making it so will be possible to move the
    boxes before they fall onto the bar.

--- Balancing Game Code.py
+++ Balancing Game Code.py
@@ -6,6 +6,31 @@ from pygame.locals import *
 import pymunk
 import pymunk.pygame_util
 
+def spawn_box(space):
+    """A funtion that controls how the boxes will spawn."""
+    mass = 10
+    h = random.randint(10, 40)
+    w = random.randint(10, 40)
+    size = (h, w)
+    intertia = pymunk.moment_for_box(mass, size)
+    body = pymunk.Body(mass, intertia)
+    body.position = (300, 500)
+    box = pymunk.Poly(body, [(-w, -h), (-w, h), (w, -h), (w, h)])
+    box.friction = 10
+    box.elasticity = 0
+    space.add(body, box)
+    return box
+
+
+
+# def add_box(space):
+#     ticks_to_next_ball = 10
+#     ticks_to_next_ball -= 1
+#     if ticks_to_next_ball <= 0:
+#         ticks_to_next_ball = 150
+#         ball_shape = spawn_box(space)
+#         balls.append(ball_shape)
+
 
 def main():
     pygame.init()
@@ -17,10 +42,8 @@ def main():
     space.gravity = (0.0, -450.0)
 
     lines = draw_balance_thing(space)
-    balls = []
     draw_options = pymunk.pygame_util.DrawOptions(screen)
 
-    ticks_to_next_ball = 10
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -28,63 +51,56 @@ def main():
             elif event.type == KEYDOWN and event.key == K_ESCAPE:
                 sys.exit(0)
             elif event.type == KEYDOWN and event.key == pygame.K_SPACE:
-                pygame.time.delay(2500)
-                
+                spawn_box(space)
+            elif event.type == KEYDOWN and event.key == pygame.K_LEFT:
+                x -= 10
+                print(x)
+            elif event.type == KEYDOWN and event.key == pygame.K_RIGHT:
+                x += 10
+                print(x)
 
-        ticks_to_next_ball -= 1
-        if ticks_to_next_ball <= 0:
-            ticks_to_next_ball = 150
-            ball_shape = spawn_box(space)
-            balls.append(ball_shape)
+        # ticks_to_next_ball -= 1
+        # if ticks_to_next_ball <= 0:
+        #     ticks_to_next_ball = 150
+        #     ball_shape = spawn_box(space)
+        #     balls.append(ball_shape)
 
         screen.fill((255, 255, 255))
 
-        balls_to_remove = []
-        for ball in balls:
-            if ball.body.position.y < 100:
-                sys.exit(0)
+        # balls_to_remove = []
+        # for ball in balls:
+        #     if ball.body.position.y < 100:
+        #         sys.exit(0)
 
-        for ball in balls_to_remove:
-            space.remove(ball, ball.body)
-            balls.remove(ball)
+        # for ball in balls_to_remove:
+        #     space.remove(ball, ball.body)
+        #     balls.remove(ball)
 
         space.debug_draw(draw_options)
 
-        space.step(1 / 50.0)
+        space.step(1 / 100.0)
 
         pygame.display.flip()
         clock.tick(50)
 
-def spawn_box(space):
-    """A funtion that controls how the boxes will spawn."""
-    mass = 10
-    h = random.randint(10, 40)
-    w = random.randint(10, 40)
-    size = (h, w)
-    intertia = pymunk.moment_for_box(mass, size)
-    x = random.randint(150, 450)
-    body = pymunk.Body(mass, intertia)
-    body.position = (x, 500)
-    box = pymunk.Poly(body, [(-w, -h), (-w, h), (w, -h), (w, h)])
-    box.friction = 10
-    space.add(body, box)
-    return box
 
 def draw_balance_thing(space):
     """Adds in the thing we will be balancing on."""
     rotation_center_body = pymunk.Body(body_type=pymunk.Body.STATIC)
-    rotation_center_body.position = (300, 300)
+    rotation_center_body.position = (300, 200)
     rotation_main_body = pymunk.Body(body_type=pymunk.Body.STATIC)
-    rotation_main_body.position = (300, 300)
+    rotation_main_body.position = (300, 200)
 
     body = pymunk.Body(10, 10000)
     body.position = (300, 200)
     l1 = pymunk.Segment(body, (-200, 0), (200, 0.0), 5.0)
-    l1.friction = 1
+    l1.friction = 10
+    l1.elasticity = 0
 
-    rotation_center_joint = pymunk.constraint.PinJoint(body, rotation_center_body, anchor_a=(0,100), anchor_b=(0,-25))
-    rotation_main_joint = pymunk.constraint.PinJoint(body, rotation_main_body, anchor_a=(0,0), anchor_b=(0,25))
-    space.add(l1, body, rotation_center_joint, rotation_main_joint)
+    rotation_center_joint = pymunk.constraint.PinJoint(body, rotation_center_body, anchor_a=(-75,0), anchor_b=(-75,25))
+    rotation_main_joint = pymunk.constraint.PinJoint(body, rotation_main_body, anchor_a=(75,0), anchor_b=(75,25))
+    joint = pymunk.constraint.PinJoint(body, rotation_main_body, anchor_a=(-75,0), anchor_b=(75,0))
+    space.add(l1, body, rotation_center_joint, rotation_main_joint, joint)
     return l1
 
 
