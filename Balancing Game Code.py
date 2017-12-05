38090fa66f33841e5c7dbe110c46665a6ae0ebb1
ZMeharg <ZMeharg@OlivetCollege.edu>
2017-12-04 20:17:05 -0500

    Balancing game
    An updated version of the game near completion, just need to fix the
    moving of the boxes so the boxes can be moved in order to keep the beam
    balanced.

--- Balancing Game Code.py
+++ Balancing Game Code.py
@@ -6,58 +6,64 @@ from pygame.locals import *
 import pymunk
 import pymunk.pygame_util
 
-def spawn_box(space):
+def spawn_small_box(space):
     """A funtion that controls how the boxes will spawn."""
     mass = 10
-    h = random.randint(10, 40)
-    w = random.randint(10, 40)
+    h = random.randint(5, 15)
+    w = h
     size = (h, w)
     intertia = pymunk.moment_for_box(mass, size)
     body = pymunk.Body(mass, intertia)
-    body.position = (300, 500)
+    body.position = 300, 500
     box = pymunk.Poly(body, [(-w, -h), (-w, h), (w, -h), (w, h)])
-    box.friction = 10
+    box.friction = 0.5
     box.elasticity = 0
     space.add(body, box)
     return box
 
-class mover:
+def spawn_medium_box(space):
+    """A funtion that controls how the boxes will spawn."""
+    mass = 10
+    h = random.randint(16, 25)
+    w = h
+    size = (h, w)
+    intertia = pymunk.moment_for_box(mass, size)
+    body = pymunk.Body(mass, intertia)
+    body.position = 300, 500
+    box = pymunk.Poly(body, [(-w, -h), (-w, h), (w, -h), (w, h)])
+    box.friction = 0.5
+    box.elasticity = 0
+    space.add(body, box)
+    return box
 
-    def __init__(self, x, y, keys):
-        self.x = x
-        self.y = y
-        self.keys = keys
-        self.up = ord(keys["up"])
-        self.down = ord(keys["down"])
-        self.left = ord(keys["left"])
-        self.right = ord(keys["right"])
-        self.origin = (x, y)
+def spawn_large_box(space):
+    """A funtion that controls how the boxes will spawn."""
+    mass = 10
+    h = random.randint(26, 40)
+    w = h
+    size = (h, w)
+    intertia = pymunk.moment_for_box(mass, size)
+    body = pymunk.Body(mass, intertia)
+    body.position = 300, 500
+    box = pymunk.Poly(body, [(-w, -h), (-w, h), (w, -h), (w, h)])
+    box.friction = 0.5
+    box.elasticity = 0
+    space.add(body, box)
+    return box
 
-    def update(self):
-        pressed = pygame.key.get_pressed()
-        if pressed[self.up]:
-            self.y -= 55
-        if pressed[self.down]:
-            self.y += 55
-        if pressed[self.left]:
-            self.x -= 55
-        if pressed[self.right]:
-            self.x += 55
-        if self.x < 20:
-            self.x = 20
-        if self.x > b:
-            self.x = b
-        if self.y < 20:
-            self.y = 20
-        if self.y > b:
-            self.y = b
+def help():
+    print("Escape: Exit")
+    print("S: Spawn Small Box")
+    print("M: Spawn Medium Box")
+    print("L: Spawn Large Box")
+    print("Left: Move Left (Not Working)")
+    print("Right: Move Right (Not Working)")
 
-mover1 = mover(100, 600, {"left": "j", "down": "k", "right": "l", "up": "i"})
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((600, 600))
-    pygame.display.set_caption("Joints. Just wait and the L will tip over")
+    pygame.display.set_caption("How well can you balance? For command help press 'c'")
     clock = pygame.time.Clock()
 
     space = pymunk.Space()
@@ -67,21 +73,40 @@ def main():
     boxs = []
 
     draw_options = pymunk.pygame_util.DrawOptions(screen)
-
-
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
                 sys.exit(0)
             elif event.type == KEYDOWN and event.key == K_ESCAPE:
                 sys.exit(0)
-            elif event.type == KEYDOWN and event.key == pygame.K_SPACE:
-                box_shape = spawn_box(space)
+            elif event.type == KEYDOWN and event.key == pygame.K_LEFT:
+                x -= 10
+            elif event.type == KEYDOWN and event.key == pygame.K_RIGHT:
+                boxs += 10
+            elif event.type == KEYDOWN and event.key == pygame.K_s:
+                box_shape = spawn_small_box(space)
                 boxs.append(box_shape)
+            elif event.type == KEYDOWN and event.key == pygame.K_m:
+                box_shape = spawn_medium_box(space)
+                boxs.append(box_shape)
+            elif event.type == KEYDOWN and event.key == pygame.K_l:
+                box_shape = spawn_large_box(space)
+                boxs.append(box_shape)
+            elif event.type == KEYDOWN and event.key == pygame.K_c:
+                help()
 
+        boxs_to_remove = []
         for box in boxs:
             if box.body.position.y < 0:
+                boxnum = len(boxs) - 1
+                print(boxnum, "of boxes were stacked successfully")
+                boxs_to_remove.append(box)
                 sys.exit(0)
+
+        for box in boxs_to_remove:
+            space.remove(box, box.body)
+            boxs.remove(box)
+
         screen.fill((255, 255, 255))
 
         space.debug_draw(draw_options)
@@ -101,11 +126,11 @@ def draw_balance_thing(space):
     body = pymunk.Body(10, 10000)
     body.position = (300, 200)
     l1 = pymunk.Segment(body, (-200, 0), (200, 0.0), 5.0)
-    l1.friction = 10
+    l1.friction = 0.5
     l1.elasticity = 0
 
-    rotation_center_joint = pymunk.constraint.PinJoint(body, rotation_center_body, anchor_a=(-75,0), anchor_b=(-75,25))
-    rotation_main_joint = pymunk.constraint.PinJoint(body, rotation_main_body, anchor_a=(75,0), anchor_b=(75,25))
+    rotation_center_joint = pymunk.constraint.SlideJoint(body, rotation_center_body, anchor_a=(-25,0), anchor_b=(-25,10), min=0, max=10)
+    rotation_main_joint = pymunk.constraint.SlideJoint(body, rotation_main_body, anchor_a=(25,0), anchor_b=(25,10), min=0, max=10)
     joint = pymunk.constraint.PinJoint(body, rotation_main_body, anchor_a=(-75,0), anchor_b=(75,0))
     space.add(l1, body, rotation_center_joint, rotation_main_joint, joint)
     return l1
